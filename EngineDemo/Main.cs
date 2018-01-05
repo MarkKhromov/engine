6a9970cd5f1631fd465bee6ddf7ff1f5f5af6d3a
Mark Khromov <lane.71rus@gmail.com>
2018-01-05 07:45:32 +0300

    Added demo project

---
+++ EngineDemo/Main.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace EngineDemo {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EngineDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EngineDemo")]
+[assembly: AssemblyProduct("EngineDemo")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("9ee1f5e9-562e-4167-865b-af6423306f73")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
