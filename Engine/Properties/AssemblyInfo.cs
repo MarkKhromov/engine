3627020d3c45448535fdf0addc10346d31767e9f
Mark Khromov <lane.71rus@gmail.com>
2018-01-05 05:25:58 +0300

    Added base project files

---
+++ Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Engine")]
+[assembly: AssemblyProduct("Engine")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c58df73c-a129-4c6a-8ca1-15a98442b6ad")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
