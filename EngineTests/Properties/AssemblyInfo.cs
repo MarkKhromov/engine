22d752bacbdcd1a4c9a570c57f71a29eee5f0884
Mark Khromov <lane.71rus@gmail.com>
2018-01-05 06:16:29 +0300

    Added tests project

---
+++ EngineTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EngineTests")]
+[assembly: AssemblyProduct("EngineTests")]
+[assembly: AssemblyCopyright("Copyright © Mark Khromov 2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5bb5d7eb-86f5-46db-a406-c5ca2b86e49a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
