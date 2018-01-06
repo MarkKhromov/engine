1176d534fc6e2a8aeb2ce9ed9a3a3c59e1c82764
Mark Khromov <lane.71rus@gmail.com>
2018-01-06 03:29:52 +0300

    Improve render coords method

--- Engine/DebugHelper.cs
+++ Engine/DebugHelper.cs
@@ -1,11 +1,12 @@
 ﻿using System.Drawing;
+using System.Globalization;
 
 namespace Engine {
     static class DebugHelper {
         public static void RenderCoords(Graphics graphics, Player player) {
             using(var font = new Font(FontFamily.GenericMonospace, 6.0f)) {
-                graphics.DrawString($"X: {player.Coords.X}", font, Brushes.White, player.Coords.X + player.Size.Width + 3.0f, player.Coords.Y);
-                graphics.DrawString($"Y: {player.Coords.Y}", font, Brushes.White, player.Coords.X + player.Size.Width + 3.0f, player.Coords.Y + 8.0f);
+                graphics.DrawString($"X: {player.Coords.X.ToString("0.00", CultureInfo.InvariantCulture)}", font, Brushes.White, player.Coords.X + player.Size.Width + 3.0f, player.Coords.Y);
+                graphics.DrawString($"Y: {player.Coords.Y.ToString("0.00", CultureInfo.InvariantCulture)}", font, Brushes.White, player.Coords.X + player.Size.Width + 3.0f, player.Coords.Y + 8.0f);
             }
         }
     }
